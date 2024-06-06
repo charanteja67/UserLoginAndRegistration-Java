a85f883cfd2375cc9f7d203f79193e1ea8e6fed5
charanteja67 <108660102+charanteja67@users.noreply.github.com>
2024-06-06 22:22:14 +0530

    Add files via upload

---
+++ Userinfo.sql
@@ -0,0 +1,5 @@
+create database Userinfo;
+use Userinfo;
+create table users(id int primary key auto_increment,uname varchar(50),upwd varchar(50),uemail varchar(50),umobile varchar(20));
+desc users;
+select*from users;
